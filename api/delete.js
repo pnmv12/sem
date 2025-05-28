2cef70e017dc77bc8899b7c7ca7483d1c810f5b3
pnmv12 <povadik12@gmail.com>
2025-05-28 16:11:19 +0200

    Update delete.js

--- api/delete.js
+++ api/delete.js
@@ -3,7 +3,7 @@ import { getDatabase } from './database.js'; // Importuje funkci getDatabase z m
 export default async function handler(request, response) { // Definuje a exportuje asynchronní funkci handler, která zpracovává HTTP požadavky.
     const database  = getDatabase();
     const klic = request.body;
-    await database.ref('napisat SVOj fajl').child(klic).remove(); // mazani dat s databaze
+    await database.ref('solution').child(klic).remove(); // mazani dat s databaze
     response.status(200).json({success: true}); // Odeslání úspěšné odpovědi
 
 }
