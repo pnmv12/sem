08ef141f7db7225abf3e03b6a4f1e5030698ef87
pnmv12 <povadik12@gmail.com>
2025-05-28 14:26:57 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     const { email, password } = req.body;
-    if(email == process.LOGIN && password == process.PASSWORD) {
+    if(email == process.env.LOGIN && password == process.env.PASSWORD) {
         res.status(200).json({ success: true });
     }else{
         res.status(401).json({ success: false, message: 'Invalid credentials' });
